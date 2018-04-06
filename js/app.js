7da67cb59cab0fcdc0d25bfa136f3a8ed5ebf756
Mustafa Wahba <mustafa.m.wahba@gmail.com>
2018-04-06 18:50:08 +0200

    fix left pane containers on scroll

---
+++ js/app.js
@@ -0,0 +1,38 @@
+var wrap = $("main");
+
+$(window).on("scroll", function(e) { 
+  console.log($(this).scrollTop());
+
+  if ($(this).scrollTop() > 343 && $(this).scrollTop() < 2327){
+    $(".main-inner").removeClass("main-inner-scroll-down");
+    $(".left").removeClass("left-scroll-down");
+    
+    $(".upper-activities").addClass("upper-activities-fixed");
+    $(".lower-activities").addClass("lower-activities-fixed");    
+
+    if ($(this).scrollTop() > 1291) {
+      $(".projects").addClass("projects-fixed");
+    } 
+    else {
+    $(".projects").removeClass("projects-fixed");
+    }
+  }
+
+  else if ($(this).scrollTop() > 2327){
+    $(".upper-activities").removeClass("upper-activities-fixed");
+    $(".lower-activities").removeClass("lower-activities-fixed");
+    $(".projects").removeClass("projects-fixed");
+    $(".main-inner").addClass("main-inner-scroll-down");
+    $(".left").addClass("left-scroll-down");
+
+  }
+
+  else {
+    $(".upper-activities").removeClass("upper-activities-fixed");
+    $(".lower-activities").removeClass("lower-activities-fixed");
+  }
+  
+});
+
+
+
