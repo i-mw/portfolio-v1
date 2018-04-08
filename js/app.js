93f2dcfa0566bd1562f8dab042c04aad57ea30ee
Mustafa Wahba <mustafa.m.wahba@gmail.com>
2018-04-08 22:44:44 +0200

    initially fix activities panes to the right while scrolling

--- js/app.js
+++ js/app.js
@@ -37,9 +37,11 @@
 
 
 
-/*set left pane height to the height of projects pane*/
+
 
 if ($(window).width() + 15 >= 600 ) {
+
+  /*set left pane height to the height of projects pane*/
   var projectsHeight = $(".projects").css("height");
   var heightNum = projectsHeight[0] + projectsHeight[1] + projectsHeight[2] + projectsHeight[3];
   heightNum = Number(heightNum);
@@ -51,13 +53,126 @@ if ($(window).width() + 15 >= 600 ) {
   var leftHeight = heightNum + marginNum + "px";
 
   $(".left").css("height", leftHeight);
+
+
+  
+  /*Set upper activities pane bottom to the height of lower activities pane*/
+  var lowerActHeight = $(".lower-activities").css("height");
+  var lowerActHeightNum = lowerActHeight[0] + lowerActHeight[1] + lowerActHeight[2];
+  lowerActHeightNum = Number(lowerActHeightNum);
+  
+  var bottom = lowerActHeightNum + "px";
+
+  $(".upper-activities").css("bottom", bottom);
+  
+
+
+    if ( $(window).scrollTop() + $(window).height() >= $(".projects").offset().top + $(".projects").height() ){
+      console.log("scroll");
+      $(".upper-activities").removeClass("upper-activities-fixed");
+      $(".lower-activities").removeClass("lower-activities-fixed");
+      $(".upper-activities").addClass("upper-activities-scroll");
+      $(".lower-activities").addClass("lower-activities-scroll"); 
+    }
+
+    else if( $(window).scrollTop() + $(window).height() >= $(".lower-activities").offset().top + $(".lower-activities").height()  &&  $(".upper-activities").offset().top >= $(".projects").offset().top   &&   $('.projects').offset().top + $(".projects").height() > $(".lower-activities").offset().top + $(".lower-activities").height()){
+      console.log("fixed");
+      $(".upper-activities").removeClass("upper-activities-scroll");
+      $(".lower-activities").removeClass("lower-activities-scroll");       
+      $(".upper-activities").addClass("upper-activities-fixed");
+      $(".lower-activities").addClass("lower-activities-fixed");
+    }
+
+
+    else {
+      console.log("above");
+      $(".upper-activities").removeClass("upper-activities-fixed");
+      $(".lower-activities").removeClass("lower-activities-fixed");      
+      $(".upper-activities").removeClass("upper-activities-scroll");
+      $(".lower-activities").removeClass("lower-activities-scroll");       
+    }
+
+
+
+
+
+
+
+
+  $(window).scroll(function(){
+
+// console.log($(window).scrollTop() + $(window).height() >= $(".lower-activities").offset().top + $(".lower-activities").height());
+// console.log($(".upper-activities").offset().top >= $(".projects").offset().top );
+// console.log($('.projects').offset().top + $(".projects").height() > $(".lower-activities").offset().top + $(".lower-activities").height());
+
+// console.log($('.projects').offset().top + $(".projects").height());
+// console.log($(".lower-activities").offset().top + $(".lower-activities").height());
+
+
+    if ( $(window).scrollTop() + $(window).height() > $(".projects").offset().top + $(".projects").height() ){
+      console.log("scroll");
+      $(".upper-activities").removeClass("upper-activities-fixed");
+      $(".lower-activities").removeClass("lower-activities-fixed");
+      $(".upper-activities").addClass("upper-activities-scroll");
+      $(".lower-activities").addClass("lower-activities-scroll"); 
+    }
+
+
+
+
+
+
+    else if( $(window).scrollTop() + $(window).height() >= $(".lower-activities").offset().top + $(".lower-activities").height()  &&  $(".upper-activities").offset().top >= $(".projects").offset().top   &&   $('.projects').offset().top + $(".projects").height() > $(".lower-activities").offset().top + $(".lower-activities").height()){
+      console.log("fixed");
+      $(".upper-activities").removeClass("upper-activities-scroll");
+      $(".lower-activities").removeClass("lower-activities-scroll");       
+      $(".upper-activities").addClass("upper-activities-fixed");
+      $(".lower-activities").addClass("lower-activities-fixed");
+    }
+
+
+
+    else {
+      console.log("above");
+
+      console.log($(window).scrollTop() + $(window).height() >= $(".lower-activities").offset().top + $(".lower-activities").height());
+      console.log($(".upper-activities").offset().top >= $(".projects").offset().top);
+      console.log($('.projects').offset().top + $(".projects").height() >= $(".lower-activities").offset().top + $(".lower-activities").height());
+
+
+
+      $(".upper-activities").removeClass("upper-activities-fixed");
+      $(".lower-activities").removeClass("lower-activities-fixed");      
+      $(".upper-activities").removeClass("upper-activities-scroll");
+      $(".lower-activities").removeClass("lower-activities-scroll");       
+    }
+
+
+
+});
+
+
+
+
 }
 else {
   $(".left").css("height", "auto");
 }
 
 
+
+/* activities panes fixation .. 2nd state */
+// if ($(window).width() + 15 >= 920 ){
+//   $(".upper-activities").css("right", $(".inner").css("margin-left"));
+//   $(".lower-activities").css("right", $(".inner").css("margin-left"));
+// }
+// else {}
+
+
+
+
 $(window).resize(function(){
+    /*set left pane height to the height of projects pane*/
   if ($(window).width() + 15 >= 600 ) {
     var projectsHeight = $(".projects").css("height");
     var heightNum = projectsHeight[0] + projectsHeight[1] + projectsHeight[2] + projectsHeight[3];
@@ -70,6 +185,34 @@ $(window).resize(function(){
     var leftHeight = heightNum + marginNum + "px";
 
     $(".left").css("height", leftHeight);
+
+
+
+    /*Set upper activities pane bottom to the height of lower activities pane*/
+    var lowerActHeight = $(".lower-activities").css("height");
+    var lowerActHeightNum = lowerActHeight[0] + lowerActHeight[1] + lowerActHeight[2];
+    lowerActHeightNum = Number(lowerActHeightNum);
+  
+    var bottom = lowerActHeightNum + "px";
+
+    $(".upper-activities").css("bottom", bottom);
+
+
+
+
+
+    console.log($(window).scrollTop() + $(window).height());
+    console.log($(".lower-activities").offset().top + $(".lower-activities").height());
+  
+
+//     if( $(window).scrollTop() + $(window).height() == $(".lower-activities").offset().top + $(".lower-activities").height()){
+//       $(".upper-activities").addClass("upper-activities-fixed");
+//       $(".lower-activities").addClass("lower-activities-fixed");
+//     }
+//     else {}
+
+
+
   }
   else {
     $(".left").css("height", "auto");
